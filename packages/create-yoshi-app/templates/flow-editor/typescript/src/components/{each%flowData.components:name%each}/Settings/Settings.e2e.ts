9e4c9d5987e7bd9411d072e80879acdd3bce531e
Artem Bordiian <bordyanartem@gmail.com>
2020-05-25 15:35:59 +0300

    Temp commit

--- packages/create-yoshi-app/templates/flow-editor/typescript/src/components/{each%flowData.components:name%each}/Settings/Settings.e2e.ts
+++ packages/create-yoshi-app/templates/flow-editor/typescript/src/components/{each%flowData.components:name%each}/Settings/Settings.e2e.ts
@@ -1,7 +1,7 @@
 describe('Settings Panel', () => {
   it('should display a section', async () => {
-    await page.goto('https://localhost:3100/settings/{%name%}');
+    await page.goto('https://localhost:3100/settings/widget0');
 
-    expect(await page.$$eval('section', sections => sections.length)).toBe(1);
+    expect(await page.$$eval('#root', tab => tab.length)).toBe(1);
   });
 });
